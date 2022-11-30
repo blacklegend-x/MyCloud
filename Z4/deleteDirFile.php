2f2a853b9f2dfb85982c46e5223b933958c0b614
blacklegend-x <65317168+blacklegend-x@users.noreply.github.com>
2022-11-30 18:36:02 +0100

    Update deleteDirFile.php

--- Z4/deleteDirFile.php
+++ Z4/deleteDirFile.php
@@ -7,7 +7,7 @@ if(is_dir($remove_file_dir)){
 	$dir_files = array_filter(glob($remove_file_dir . "*"));
 	foreach ($dir_files as $files) {
 		unlink($files);
-		$link = mysqli_connect('mariadb106.server701675.nazwa.pl', 'server701675_bargra1', 'N8CrQi!qb@y3YT@', 'server701675_bargra1'); //polaczenie z baza danych
+		$link = mysqli_connect(); //polaczenie z baza danych
 		if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } //obsługa błędu połączenia z BD
 		$query = "DELETE FROM file WHERE file_path = '$files'";
 		mysqli_query($link, $query);
@@ -17,7 +17,7 @@ if(is_dir($remove_file_dir)){
 }
 else{
 	unlink($remove_file_dir);
-	$link = mysqli_connect('mariadb106.server701675.nazwa.pl', 'server701675_bargra1', 'N8CrQi!qb@y3YT@', 'server701675_bargra1'); //polaczenie z baza danych
+	$link = mysqli_connect(); //polaczenie z baza danych
 	if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } //obsługa błędu połączenia z BD
 	$query = "DELETE FROM file WHERE file_path = '$remove_file_dir'";
 	mysqli_query($link, $query);
